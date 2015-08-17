7611fa2f3151dec63739be40cf50ff8036e3fbbe
Carmen Wick <CarmenWick@gmail.com>
2015-08-17 13:11:33 -0700

    Better circle builder

--- Assets/Scripts/CircleBuilder.cs
+++ Assets/Scripts/CircleBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.EventSystems;
+using System.Collections;
+using System.Linq;
 
 public interface IMeshChangedHandler : IEventSystemHandler
 {
@@ -13,6 +14,10 @@ public class CircleBuilder : MonoBehaviour
     public int vertexCount = 10;
     public float radius = 1;
     public Vector2 pivot = Vector2.zero;
+    [Range(0, 360)]
+    public float
+        rotation = 0;
+    public Vector2 scale = Vector2.one;
 
     #region Messages
 
@@ -37,7 +42,7 @@ public class CircleBuilder : MonoBehaviour
         var filter = GetComponent<MeshFilter>();
         var mesh = new Mesh();
 
-        mesh.vertices = BuildVertices();
+        mesh.vertices = TransformVertices(BuildVertices());
         mesh.triangles = BuildTriangles();
         mesh.RecalculateNormals();
         filter.sharedMesh = mesh;
@@ -51,14 +56,10 @@ public class CircleBuilder : MonoBehaviour
 
         for (var i=0; i<vertexCount; i++) {
             var t = i * angleStep;
-            var x = radius * Mathf.Cos(t) + pivot.x*radius;
-            var y = radius * Mathf.Sin(t) + pivot.y*radius;
+            var x = radius * Mathf.Cos(t);
+            var y = radius * Mathf.Sin(t);
 
-            if (HasOddVertexCount) {
-                vertices [i] = new Vector3(y, x, 0); 
-            } else {
-                vertices [i] = new Vector3(x, y, 0); 
-            }
+            vertices [i] = new Vector3(x, y, 0);
         }
 
         return vertices;
@@ -72,11 +73,22 @@ public class CircleBuilder : MonoBehaviour
         for (var i=0; i<triangleCount; i++) {
             var offset = i * 3;
             triangles [offset] = 0;
-            triangles [offset + 1] = HasOddVertexCount ? i + 2 : i + 1;
-            triangles [offset + 2] = HasOddVertexCount ? i + 1 : i + 2;
+            triangles [offset + 1] = i + 1;
+            triangles [offset + 2] = i + 2;
         }
 
         return triangles;
+    }
+
+    Vector3[] TransformVertices(Vector3[] vertices)
+    {
+        return vertices.Select((v) => {
+            var translation = pivot*radius;
+            var rotationAngle = HasOddVertexCount ? this.rotation + 90 : this.rotation;
+            var rotationQuaternion = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
+            var scale = this.scale;
+            return Matrix4x4.TRS(translation, rotationQuaternion, scale).MultiplyPoint3x4(v);
+        }).ToArray();
     }
 
     bool HasOddVertexCount {
