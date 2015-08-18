4307565914da6e413ad42acbfb3cffa42d23a185
Carmen Wick <CarmenWick@gmail.com>
2015-08-17 23:45:09 -0700

    Size mode

--- Assets/Scripts/CircleBuilder.cs
+++ Assets/Scripts/CircleBuilder.cs
@@ -1,12 +1,50 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEditor;
 using System.Collections;
 using System.Linq;
+
+[CustomEditor(typeof(CircleBuilder))]
+public class FooEditor : Editor {
+    private string[] HIDDEN_PROPERTIES = { "radius", "size", "sizingMethod" };
+
+    public override void OnInspectorGUI() {
+        serializedObject.Update();
+
+        var serializedProperty = serializedObject.GetIterator();
+        if (serializedProperty.NextVisible(true)) {
+            do {
+                if (!HIDDEN_PROPERTIES.Contains(serializedProperty.name)) {
+                    EditorGUILayout.PropertyField(serializedProperty);
+                }
+            } while(serializedProperty.NextVisible(false));
+        }
+
+        var sizingMethodProperty = serializedObject.FindProperty("sizingMethod");
+        var sizing = (PolygonSizing)sizingMethodProperty.enumValueIndex;
+        EditorGUILayout.PropertyField(sizingMethodProperty);
+
+        switch (sizing) {
+            case PolygonSizing.Bounds:
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("size"));
+                break;
+            case PolygonSizing.Radius:
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("radius"));
+                break;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    } 
+}
 
 public interface IMeshChangedHandler : IEventSystemHandler
 {
     void OnMeshChanged();
 }
+
+public enum PolygonSizing {
+    Radius, Bounds
+};
 
 [RequireComponent(typeof(MeshFilter))]
 public class CircleBuilder : MonoBehaviour
@@ -18,6 +56,7 @@ public class CircleBuilder : MonoBehaviour
     [Range(0, 360)]
     public float rotation = 0;
     public Vector2 scale = Vector2.one;
+    public PolygonSizing sizingMethod = PolygonSizing.Radius;
 
     #region Messages
 
@@ -83,16 +122,37 @@ public class CircleBuilder : MonoBehaviour
 
     Vector3[] TransformVertices(Vector3[] vertices)
     {
-        var bounds = CalculateBounds(vertices);
-        var scaleFactor = new Vector2(size.x / bounds.size.x, size.y / bounds.size.y);
+        var scaleFactor = CalculateScale(vertices);
+        var translation = CalculateTranslation();
+        var rotationQuaternion = Quaternion.AngleAxis(rotation, Vector3.forward);
+        var transform = Matrix4x4.TRS(translation, rotationQuaternion, scaleFactor);
 
         return vertices.Select((v) => {
-            var translation = pivot * radius;
-            var rotationQuaternion = Quaternion.AngleAxis(rotation, Vector3.forward);
-            var scale = this.scale * radius;
-            scale.Scale(scaleFactor);
-            return Matrix4x4.TRS(translation, rotationQuaternion, scale).MultiplyPoint3x4(v);
+            return transform.MultiplyPoint3x4(v);
         }).ToArray();
+    }
+
+    Vector2 CalculateTranslation() 
+    {
+        var translation = pivot;
+        var factor = sizingMethod == PolygonSizing.Bounds ? size : new Vector2(radius, radius);
+        translation.Scale(factor);
+        return translation;
+    }
+
+    Vector2 CalculateScale(Vector3[] vertices) 
+    {
+        Vector2 scaleFactor;
+
+        if (sizingMethod == PolygonSizing.Bounds) {
+            var bounds = CalculateBounds(vertices);
+            scaleFactor = new Vector2(size.x / bounds.size.x, size.y / bounds.size.y);
+        } else {
+            scaleFactor = new Vector2(radius, radius);
+        }
+
+        scaleFactor.Scale(this.scale);
+        return scaleFactor;
     }
 
     Bounds CalculateBounds(Vector3[] vertices)
