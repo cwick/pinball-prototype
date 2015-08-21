f591a51920690872bd5636e230f1cf3ff0c6545b
Carmen Wick <CarmenWick@gmail.com>
2015-08-21 00:45:47 -0700

    Add missing files

---
+++ Assets/Editor/PolygonGeneratorEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(PolygonGenerator))]
+public class PolygonGeneratorEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        var generator = (PolygonGenerator)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate")) {
+            generator.Generate();
+        }
+    }
+}
